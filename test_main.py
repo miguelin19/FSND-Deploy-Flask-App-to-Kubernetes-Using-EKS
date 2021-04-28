ba4600b13fb263fcfb6212a2b727fd86c77839e5
miguelin19 <miguel_carra19@hotmail.com>
2021-04-28 19:52:34 +0200

    final

--- test_main.py
+++ test_main.py
@@ -32,7 +32,7 @@ def test_auth(client):
     body = {'email': EMAIL,
             'password': PASSWORD}
     response = client.post('/auth', 
-                           data=json(body),
+                           data=json.dumps(body),
                            content_type='application/json')
 
     assert response.status_code == 200
